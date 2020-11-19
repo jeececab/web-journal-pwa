08b6ad4288aeee920f2c07737c720980e8898a4c
jeececab <jcseguincabana@gmail.com>
2020-11-18 19:00:06 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="container mx-auto bg-gray-800">
+      <h1 className="font-sans text-5xl text-gray-50">Hello world</h1>
+    </div>
+  );
+};
+
+export default App;
