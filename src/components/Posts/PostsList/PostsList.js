4f9a5de2709b9d434b4a097ede570f9787b444a4
jeececab <jcseguincabana@gmail.com>
2020-11-24 12:21:54 -0500

    no post

--- src/components/Posts/PostsList/PostsList.js
+++ src/components/Posts/PostsList/PostsList.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { fetchUserPosts } from '../../../api/post';
 import PostsContext from '../../../contexts/PostsContext';
 import LoadingSpinner from '../../LoadingSpinner';
@@ -38,7 +39,19 @@ const PostsList = () => {
 
       {!loading && error && <p>{error}</p>}
 
-      {!loading && (
+      {!loading && posts.length === 0 && (
+        <div className="container mx-auto text-center text-xl mt-2">
+          <p>You don't have any post yet</p>
+          <Link
+            className="block w-40 font-bold border-green-600 hover:border-green-500 border-2 my-4 mx-auto py-2 bg-green-600 hover:bg-green-500 text-gray-800"
+            to="/posts/new"
+          >
+            Add post
+          </Link>
+        </div>
+      )}
+
+      {!loading && posts.length > 0 && (
         <PostsContext.Provider value={{ posts, setPostsList, meta, options }}>
           <PostsListTable />
           <Pagination />
